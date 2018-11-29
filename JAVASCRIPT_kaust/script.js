620e9cbb60ac26bdb0b1e6fc1e5b61634996f646
Konstantin <konstantin-r@hotmail.com>
2018-11-29 22:58:34 +0200

    Remove html errors and css errors,javascript

--- JAVASCRIPT_kaust/script.js
+++ JAVASCRIPT_kaust/script.js
@@ -1,13 +1,17 @@
 function myFunction() {
+    var element = document.getElementById("graf");
+
+    //var txtFile = "/home/leesikas01/m/maier/public_html/32/dist/login_details.txt";
+    
+
     document.getElementById("graf").style.transition = "1s";
     document.getElementById("graf").style.visibility = "visible";
     document.getElementById("graf").style.display = "block";
     document.getElementById("graf").style.webkitTransition = 'opacity 1s';
 
-    var element = document.getElementById("graf");
 
 
     element.scrollIntoView({block:"center"}); // center
-    document.getElementById("graf").style.opacity = "1";
+    element.style.opacity = "1";
 
 }
