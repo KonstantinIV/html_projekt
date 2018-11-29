ff7b41fe54ef0dac712dca564647b5009e58f44f
Hain <hainluud@eesti.ee>
2018-11-29 14:11:54 +0200

    s

--- JAVASCRIPT_kaust/script.js
+++ JAVASCRIPT_kaust/script.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-function runPython() {
-    WshShell = new ActiveXObject("WScript.Shell");
-    WshShell.Run("\\\\https://kodu.ut.ee\\~hainluud\\dist\\webscrape.exe",1,true);
-    }
-=======
 function myFunction() {
     document.getElementById("graf").style.transition = "1s";
     document.getElementById("graf").style.visibility = "visible";
@@ -17,4 +11,3 @@ function myFunction() {
     document.getElementById("graf").style.opacity = "1";
 
 }
->>>>>>> e43e33b5e0c04cea3bdc6afa3086f0999a786035
